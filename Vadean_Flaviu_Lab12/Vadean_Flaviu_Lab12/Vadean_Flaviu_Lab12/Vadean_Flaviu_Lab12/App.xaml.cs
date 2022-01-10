ff9ce0871ac393da8392b1f004cb36c06675303d
VadeanFlaviu <flaviu@metagamestudio.com>
2022-01-10 16:04:23 +0200

    Add project files.

---
+++ Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Vadean_Flaviu_Lab12.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vadean_Flaviu_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/Vadean_Flaviu_Lab12/Data/IRestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vadean_Flaviu_Lab12.Models;
+
+namespace Vadean_Flaviu_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
